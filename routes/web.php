dd9eb05af7feab4163d568ce43c18eefa5c0f411
shreevarshaafeather-eng <shreevarshaafeather@gmail.com>
2025-11-17 14:16:35 +0530

    .
    .

--- routes/web.php
+++ routes/web.php
@@ -4,56 +4,44 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CommissionController;
+use App\Http\Controllers\CancelRequestController;
+use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\SettingsController;
 
 
-use App\Http\Controllers\CancelRequestController;
 
 
 Route::get('/', function () {
-    return redirect()->route('login');
+    return redirect()->route('company.setup');
 });
 
 
-// Commission Routes
-Route::get('/commission', [CommissionController::class, 'index'])->name('commission.index');
 
-// Create (Add)
-Route::get('/commission/create', [CommissionController::class, 'create'])->name('commission.create');
-Route::post('/commission/store', [CommissionController::class, 'store'])->name('commission.store');
+Route::get('/setup', [CompanyController::class, 'index'])->name('company.setup');
+Route::post('/setup', [CompanyController::class, 'store'])->name('company.store');
 
-// Edit
-Route::get('/commission/edit/{id}', [CommissionController::class, 'edit'])->name('commission.edit');
-Route::post('/commission/update/{id}', [CommissionController::class, 'update'])->name('commission.update');
+Route::get('/select-role', function () {
+    return view('select-role');
+})->name('select.role');
 
-// Delete
-Route::delete('/commission/delete/{id}', [CommissionController::class, 'destroy'])->name('commission.delete');
 
 
 Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
 Route::post('/register', [AuthController::class, 'register'])->name('register.post');
+
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login'])->name('login.post');
+
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-
-Route::get('/cancel-request', [CancelRequestController::class, 'index'])->name('cancel.index');
-Route::post('/cancel-request/store', [CancelRequestController::class, 'store'])->name('cancel.store');
-
-Route::get('/cancel-request/edit/{id}', [CancelRequestController::class, 'edit'])->name('cancel.edit');
-Route::post('/cancel-request/update/{id}', [CancelRequestController::class, 'update'])->name('cancel.update');
-
-Route::delete('/cancel-request/delete/{id}', [CancelRequestController::class, 'destroy'])->name('cancel.delete');
 
 
 Route::middleware(['auth'])->group(function () {
+
+    // Dashboard
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
-});
-
-Route::middleware(['auth'])->group(function () {
-
-    Route::get('/settings', [SettingsController::class, 'index'])->name('settings');
+Route::get('/settings', [SettingsController::class, 'index'])->name('settings');
 
     Route::post('/settings/profile', [SettingsController::class, 'updateProfileImage'])->name('settings.profile');
 
@@ -62,7 +50,24 @@ Route::middleware(['auth'])->group(function () {
     Route::post('/settings/account', [SettingsController::class, 'updateAccount'])->name('settings.account');
 
     Route::delete('/settings/delete', [SettingsController::class, 'deleteAccount'])->name('settings.delete');
+   
+
+    Route::get('/commission', [CommissionController::class, 'index'])->name('commission.index');
+
+    Route::get('/commission/create', [CommissionController::class, 'create'])->name('commission.create');
+    Route::post('/commission/store', [CommissionController::class, 'store'])->name('commission.store');
+
+    Route::get('/commission/edit/{id}', [CommissionController::class, 'edit'])->name('commission.edit');
+    Route::post('/commission/update/{id}', [CommissionController::class, 'update'])->name('commission.update');
+
+    Route::delete('/commission/delete/{id}', [CommissionController::class, 'destroy'])->name('commission.delete');
+
+    Route::get('/cancel-request', [CancelRequestController::class, 'index'])->name('cancel.index');
+    Route::post('/cancel-request/store', [CancelRequestController::class, 'store'])->name('cancel.store');
+
+    Route::get('/cancel-request/edit/{id}', [CancelRequestController::class, 'edit'])->name('cancel.edit');
+    Route::post('/cancel-request/update/{id}', [CancelRequestController::class, 'update'])->name('cancel.update');
+
+    Route::delete('/cancel-request/delete/{id}', [CancelRequestController::class, 'destroy'])->name('cancel.delete');
 
 });
-
-
